7854aaa9cacd0afdf51f10f17a2759e93ed30872
Şenol Ataç <senol.atac@nar.com.tr>
2024-02-12 11:09:15 +0300

    initial

---
+++ src/test/java/com/nar/hexademo/CoreTest.java
@@ -0,0 +1,16 @@
+package com.nar.hexademo;
+
+import com.sha.shacore.util.NumberUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CoreTest {
+
+    @Test
+    void test_multiply() {
+        int r = NumberUtils.multiply(4, 5);
+
+        assertThat(r).isEqualTo(20);
+    }
+}
